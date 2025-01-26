b0f84d9de06415a2081181dc443e2a39fc24cdbe
Mister K <678459+kairin@users.noreply.github.com>
2025-01-27 05:29:20 +0800

    Update OpenCV-4-11-0.sh
    python3-dev is sufficient

--- OpenCV-4-11-0.sh
+++ OpenCV-4-11-0.sh
@@ -48,7 +48,7 @@ install_opencv () {
   # if [[ $model == *"Jetson Nano"* ]]; then
     sudo apt-get install -y build-essential git unzip pkg-config zlib1g-dev
     sudo apt-get install -y python3-dev python3-numpy
-    sudo apt-get install -y python-dev python-numpy
+    #sudo apt-get install -y python-dev python-numpy kairin- this line causes to hang on agx orin.
     sudo apt-get install -y gstreamer1.0-tools libgstreamer-plugins-base1.0-dev
     sudo apt-get install -y libgstreamer-plugins-good1.0-dev
     sudo apt-get install -y libtbb2 libgtk-3-dev v4l2ucp libxine2-dev
